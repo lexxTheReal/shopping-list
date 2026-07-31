f9e8f6b46056b0cd98727de6514516fc83d0d806
Lexx <alex@urwanisch.com>
2026-07-31 21:57:09 +0200

    user can new enter the name of what they want to add

--- script.js
+++ script.js
@@ -7,24 +7,43 @@ function updateContainerHeight() {
     userIndicator.style.display = count === 0 ? 'block' : 'none';
 }
 
+let counter = 0;
+
 function addItem() {
+    counter += 1;
     const container = document.getElementById('item-container');
+    const userInputField = document.getElementById('userInput');
+    const userInput = userInputField.value;
 
-    const item = document.createElement('div');
-    item.className = 'item';
-    item.tabIndex = 0;
-    item.textContent = 'Neues Item';
+    
 
-    item.addEventListener('click', () => {
-        item.style.textDecoration = 'line-through';
-        setTimeout(() => {
-            item.remove();
-            updateContainerHeight();
-            alert(item.textContent + " has been removed!")
-        }, 750)
-    });
-    container.appendChild(item);
-    updateContainerHeight();
+    if (counter % 2 != 0) { //wenn zum ersten mal im zweier cyrcle geclicked wird
+        userInputField.style.display = 'flex';
+    }
+    else{ //wenn zum ersten mal im zweier cyrcle geclicked wurde
+        userInputField.style.display = 'none';
+        
+        const item = document.createElement('div');
+        item.className = 'item';
+        item.tabIndex = 0;
+        if(userInput != "") {
+            item.textContent = userInput;
+        }
+        else{
+            item.textContent = 'NED SO NICE BRO';
+        }
+
+        item.addEventListener('click', () => {
+            item.style.textDecoration = 'line-through';
+            setTimeout(() => {
+                item.remove();
+                updateContainerHeight();
+                //alert(item.textContent + " has been removed!")
+            }, 750)
+        });
+        container.appendChild(item);
+        updateContainerHeight();
+        userInputField.value = "";
+    }
 }
-
-updateContainerHeight()
+updateContainerHeight();
